544fbb4627a3bd52e6631fc9a486e2563118ccff
Anastas Fonotov <anastasf@gmail.com>
2016-10-19 16:28:09 +0300

    fix information dialog styles

--- src/information/information.js
+++ src/information/information.js
@@ -35,7 +35,7 @@
     thisModule.controller('pipInformationDialogController',
         function ($scope, $rootScope, $mdDialog, $injector, params) {
             var content = params.message, item;
-
+console.log('content', content);
             var pipTranslate = $injector.has('pipTranslate') ? $injector.get('pipTranslate') : null;
             if (pipTranslate) {
                 pipTranslate.translations('en', {
@@ -52,16 +52,17 @@
                 $scope.title = params.title || 'Information';
                 $scope.ok = params.ok || 'OK';
             }
-
+console.log('content1', content);
             var pipFormat = $injector.has('pipFormat') ? $injector.get('pipFormat') : null;
 
             $scope.theme = $rootScope.$theme;
             if (params.item && pipFormat) {
                 item = _.truncate(params.item, 25);
                 content = pipFormat.sprintf(content, item);
+                console.log('content2', content);
             }
             $scope.content = content;
-
+console.log('content1', $scope.content);
             $scope.onOk = function () {
                 $mdDialog.hide();
             };
